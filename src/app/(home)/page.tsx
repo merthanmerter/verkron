8f55178caabff9d734b3a80927b9685ec1b9fbae
merthanmerter <merthanm@gmail.com>
2025-07-14 10:39:00 +0300

    Update main page styling: Added horizontal padding to the main container for improved layout and responsiveness.

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,7 @@
 import Particles from '@/components/particles';
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
+    <main className="flex min-h-screen flex-col items-center justify-center px-4">
       <Particles />
     </main>
   );
